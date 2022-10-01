45709b0fad21f2e3478401f43b8ed0b6d6934c50
SezerKaynak <sezerkaynak00@gmail.com>
2022-10-01 16:00:28 +0300

    firebase auth

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/service/i_auth_service.dart';
+import 'package:flutter_application_1/widgets/auth_widget_builder.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final _authService = Provider.of<IAuthService>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Page"),
+        title: const Text(""),
+        actions: [IconButton(onPressed: ()async {await _authService.signOut();}, icon: const Icon(Icons.exit_to_app))],
       ),
     );
   }
