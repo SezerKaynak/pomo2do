53ff2a0a6cbf7b954439ed9d7515ee44eb8dc019
Gökalp Akoğlu <181805035@stu.adu.edu.tr>
2022-10-04 16:38:39 +0300

    settings

--- lib/pages/person_info.dart
+++ lib/pages/person_info.dart
@@ -25,23 +25,49 @@ class PersonInfo extends StatelessWidget with ProjectThemeOptions {
                   ModalRoute.withName("/Task"));
             }),
       ),
-      body: Padding(
-        padding: ScreenPadding().screenPadding,
-        child: Column(
-          children: [
-            SizedBox(
-                width: 400,
-                height: 60,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20))),
-                    onPressed: () async {
-                      await _authService.signOut();
-                    },
-                    child: const Text("Çıkış Yap"))),
-          ],
-        ),
+      body: Column(
+        children: const [
+          TaskAdded(
+            title: "Profil İsmi",
+            subtitle: "Profilinizi düzenleyebilirsiniz",
+            //onTap: () {},
+          ),
+          TaskAdded(
+            title: "Şifreyi Değiştir",
+            subtitle: "Şifrenizi Değiştirebilirsiniz",
+            //onTap: () {},
+          ),
+          TaskAdded(
+            title: "Bildirim Ayarı",
+            subtitle:
+                "Bu kısımda almak istediğiniz bildirimleri seçebilirsiniz",
+            //onTap: () {},
+          ),
+          TaskAdded(
+            title: "Pomodoro Ayarı",
+            subtitle:
+                "Bu kısımda pomodoro ve ara dakikalarını,sayısını değiştirebilirsiniz",
+            //onTap: () {},
+          ),
+          TaskAdded(
+            title: "Çıkış Yap",
+            subtitle: "Hesaptan çıkış yapın",
+            // onTap: () async {
+            //   await _authService.signOut();
+            // },
+          ),
+          // SizedBox(
+          //     width: 400,
+          //     height: 60,
+          //     child: ElevatedButton(
+          //         style: ElevatedButton.styleFrom(
+          //             shape: RoundedRectangleBorder(
+          //                 borderRadius: BorderRadius.circular(20))),
+          //         onPressed: () async {
+          //           await _authService.signOut();
+          //         },
+          //         child: const Text("Çıkış Yap"))),
+        ],
       ),
     );
   }
