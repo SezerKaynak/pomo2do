fe01f57f490b01035041867a1a22d034cd9479d8
SezerKaynak <sezerkaynak00@gmail.com>
2022-10-19 12:30:12 +0300

    multiple choice added to completed tasks

--- lib/pages/completed_tasks.dart
+++ lib/pages/completed_tasks.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/task_model.dart';
 
-
 class CompletedTasks extends StatefulWidget {
   const CompletedTasks({super.key});
 
@@ -12,7 +11,7 @@ class CompletedTasks extends StatefulWidget {
 }
 
 class _CompletedTasksState extends State<CompletedTasks> {
-  
+  List selectedIndexes = [];
   @override
   Widget build(BuildContext context) {
     List<TaskModel> tasks =
@@ -27,53 +26,97 @@ class _CompletedTasksState extends State<CompletedTasks> {
               Navigator.pop(context);
             }),
       ),
-      body: ListView.builder(
-        itemCount: tasks.length,
-        itemBuilder: (context, index) {
-          final TaskModel data = tasks[index];
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.green[100],
-                      borderRadius: BorderRadius.circular(16.0)),
-                  child: ListTile(
-                    contentPadding: const EdgeInsets.all(15),
-                    leading: const Icon(Icons.numbers),
-                    onTap: () async {
-                          CollectionReference users = FirebaseFirestore.instance
-                              .collection(
-                                  'Users/${FirebaseAuth.instance.currentUser!.uid}/tasks');
-                          var task = users.doc(data.id);
-                          task.set({
-                            "taskName" : data.taskName,
-                            "taskInfo" : data.taskInfo,
-                            "taskType" : data.taskType,
-                            "taskNameCaseInsensitive" : data.taskName.toLowerCase(),
-                            "isDone": false,
-                          });
-                          tasks.removeAt(index);
-                          setState(() {});
-                        },
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    title: Text(data.taskName),
-                    subtitle: Text(data.taskInfo),
-                    trailing: const Icon(Icons.arrow_right_sharp),
-                  ),
-                ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.separated(
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 10),
+                itemCount: tasks.length,
+                itemBuilder: (context, index) {
+                  final TaskModel data = tasks[index];
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.green[100],
+                            borderRadius: BorderRadius.circular(16.0)),
+                        child: ListTile(
+                          contentPadding: const EdgeInsets.all(15),
+                          leading: Checkbox(
+                            value: selectedIndexes.contains(index),
+                            onChanged: (_) {
+                              setState(() {
+                                if (selectedIndexes.contains(index)) {
+                                  selectedIndexes.remove(index);
+                                } else {
+                                  selectedIndexes.add(index);
+                                }
+                              });
+                            },
+                          ),
+                          onTap: () async {
+                            setState(() {
+                              if (selectedIndexes.contains(index)) {
+                                selectedIndexes.remove(index);
+                              } else {
+                                selectedIndexes.add(index);
+                              }
+                            });
+                          },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          title: Text(data.taskName),
+                          subtitle: Text(data.taskInfo),
+                          trailing: const Icon(Icons.arrow_right_sharp),
+                        ),
+                      ),
+                    ],
+                  );
+                },
               ),
-            ],
-          );
-        },
+            ),
+            Expanded(
+              flex: 0,
+              child: SizedBox(
+                width: 400,
+                height: 60,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    onPressed: () {
+                      for (int i = 0; i < selectedIndexes.length; i++) {
+                        final TaskModel data = tasks[selectedIndexes[i]];
+                        CollectionReference users = FirebaseFirestore.instance
+                            .collection(
+                                'Users/${FirebaseAuth.instance.currentUser!.uid}/tasks');
+                        var task = users.doc(data.id);
+                        task.set({
+                          "taskName": data.taskName,
+                          "taskInfo": data.taskInfo,
+                          "taskType": data.taskType,
+                          "taskNameCaseInsensitive":
+                              data.taskName.toLowerCase(),
+                          "isDone": false,
+                        });
+                      }
+                      tasks.removeRange(0, selectedIndexes.length);
+                      setState(() {});
+                    },
+                    child: const Text(
+                        "Seçili görevleri tamamlanmamış olarak işaretle")),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
