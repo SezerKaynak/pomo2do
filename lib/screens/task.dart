af3caf1bfa02958b59efe5d9f45f22702724c7e2
SezerKaynak <sezerkaynak00@gmail.com>
2022-12-19 02:04:13 +0300

    Drawer moved into custom_drawer

--- lib/screens/task.dart
+++ lib/screens/task.dart
@@ -1,25 +1,17 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/models/pomotodo_user.dart';
 import 'package:flutter_application_1/models/task_model.dart';
 import 'package:flutter_application_1/screens/add_task.dart';
-import 'package:flutter_application_1/screens/edit_password.dart';
 import 'package:flutter_application_1/screens/pomodoro.dart';
-import 'package:flutter_application_1/screens/pomodoro_settings.dart';
 import 'package:flutter_application_1/screens/search_view.dart';
 import 'package:flutter_application_1/project_theme_options.dart';
-import 'package:flutter_application_1/providers/dark_theme_provider.dart';
-import 'package:flutter_application_1/widgets/custom_switch.dart';
-import 'package:flutter_application_1/widgets/settings.dart' as settings;
+import 'package:flutter_application_1/widgets/custom_drawer.dart';
 import 'package:flutter_application_1/service/database_service.dart';
-import 'package:flutter_application_1/service/i_auth_service.dart';
 import 'package:flutter_application_1/providers/pomodoro_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:collection/collection.dart';
 import 'package:quickalert/quickalert.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:flutter_application_1/assets/constants.dart';
 
 class TaskView extends StatefulWidget with ProjectThemeOptions {
   TaskView({Key? key}) : super(key: key);
@@ -35,41 +27,15 @@ class Task extends State<TaskView> {
   List<TaskModel>? tasks;
   final TextEditingController textController = TextEditingController();
   List<TaskModel> deletedTasks = [];
-  String? downloadUrl;
   DatabaseService dbService = DatabaseService();
   @override
   void initState() {
     super.initState();
     _initRetrieval();
-    WidgetsBinding.instance.addPostFrameCallback((_) => baglantiAl());
   }
-
-  baglantiAl() async {
-    String yol = await FirebaseStorage.instance
-        .ref()
-        .child("profilresimleri")
-        .child(context.read<PomotodoUser>().userId)
-        .child("profilResmi.png")
-        .getDownloadURL();
-
-    setState(() {
-      downloadUrl = yol;
-    });
-  }
-
-  String alertTitle = "Görev Çöp Kutusuna Taşınacak!";
-  String alertSubtitle = "Görev çöp kutusuna taşınsın mı?";
-  String alertApprove = "Onayla";
-  String alertReject = "İptal Et";
-  String alertTitleLogOut = "Çıkış Yapılacak!";
-  String alertSubtitleLogOut = "Çıkış yapmak istediğinizden emin misiniz?";
 
   @override
   Widget build(BuildContext context) {
-    final themeChange = Provider.of<DarkThemeProvider>(context);
-    CollectionReference users = FirebaseFirestore.instance.collection("Users");
-    var user = users.doc(context.read<PomotodoUser>().userId);
-    var _authService = Provider.of<IAuthService>(context, listen: false);
     return Scaffold(
         appBar: AppBar(
             title: const Text("PomoTodo",
@@ -89,172 +55,7 @@ class Task extends State<TaskView> {
                   },
                   icon: const Icon(Icons.search))
             ]),
-        drawer: Drawer(
-          width: MediaQuery.of(context).size.width * 0.745,
-          child: Column(
-            children: [
-              DrawerHeader(
-                  decoration: const BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Column(
-                    children: [
-                      Expanded(
-                        flex: 0,
-                        child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(5),
-                                child: CircleAvatar(
-                                  radius: 50.0,
-                                  child: downloadUrl != null
-                                      ? CachedNetworkImage(
-                                          fit: BoxFit.cover,
-                                          imageUrl: downloadUrl!,
-                                          imageBuilder:
-                                              (context, imageProvider) {
-                                            return ClipOval(
-                                                child: SizedBox.fromSize(
-                                                    size: const Size.fromRadius(
-                                                        50),
-                                                    child: Image(
-                                                        image: imageProvider,
-                                                        fit: BoxFit.cover)));
-                                          },
-                                          placeholder: (context, url) {
-                                            return ClipOval(
-                                                child: SizedBox.fromSize(
-                                              size: const Size.fromRadius(20),
-                                              child:
-                                                  const CircularProgressIndicator(
-                                                      color: Colors.white),
-                                            ));
-                                          },
-                                          errorWidget: (context, url, error) =>
-                                              const Icon(Icons.error),
-                                        )
-                                      : ClipOval(
-                                          child: SizedBox.fromSize(
-                                            size: const Size.fromRadius(70),
-                                            child: Image.asset(
-                                              'assets/person.png',
-                                              fit: BoxFit.cover,
-                                            ),
-                                          ),
-                                        ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(left: 8.0),
-                                  child: StreamBuilder(
-                                      stream: user.snapshots(),
-                                      builder: (BuildContext context,
-                                          AsyncSnapshot asyncSnapshot) {
-                                        if (asyncSnapshot.hasError) {
-                                          return const Text(
-                                              "Bir şeyler yanlış gitti");
-                                        } else if (asyncSnapshot.hasData &&
-                                            !asyncSnapshot.data!.exists) {
-                                          return const Text(
-                                            "Hesap ayarları sayfasından profil resmi seçebilirsiniz",
-                                            overflow: TextOverflow.clip,
-                                            maxLines: 3,
-                                          );
-                                        } else if (asyncSnapshot
-                                                .connectionState ==
-                                            ConnectionState.active) {
-                                          return Text(
-                                            "${asyncSnapshot.data.data()["name"]}"
-                                            " ${asyncSnapshot.data.data()["surname"]}",
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline4
-                                                ?.copyWith(
-                                                    fontWeight: FontWeight.w600,
-                                                    fontSize: 26,
-                                                    color: Colors.black),
-                                          );
-                                        }
-                                        return const Text("Loading");
-                                      }),
-                                ),
-                              ),
-                            ]),
-                      ),
-                    ],
-                  )),
-              settings.Settings(
-                settingIcon: Icons.account_circle,
-                title: settingTitle(context, "Hesap Ayarları"),
-                subtitle: "Profilinizi düzenleyebilirsiniz.",
-                tap: () {
-                  Navigator.pushNamed(context, '/editProfile');
-                },
-              ),
-              const Divider(thickness: 1),
-              settings.Settings(
-                settingIcon: Icons.password,
-                subtitle: "Şifrenizi değiştirebilirsiniz.",
-                title: settingTitle(context, 'Şifreyi Değiştir'),
-                tap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const EditPassword()));
-                },
-              ),
-              const Divider(thickness: 1),
-              settings.Settings(
-                  settingIcon: Icons.notifications,
-                  subtitle: "Bildirim ayarlarını yapabilirsiniz.",
-                  title: settingTitle(context, 'Bildirim Ayarları'),
-                  tap: () {}),
-              const Divider(thickness: 1),
-              settings.Settings(
-                  settingIcon: Icons.watch,
-                  subtitle: "Pomodoro sayacı vb. ayarları yapabilirsiniz.",
-                  title: settingTitle(context, 'Pomodoro Ayarları'),
-                  tap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const PomodoroSettings()));
-                  }),
-              const Divider(thickness: 1),
-              settings.Settings(
-                  settingIcon: Icons.logout,
-                  subtitle: "Hesaptan çıkış yapın.",
-                  title: settingTitle(context, 'Çıkış Yap'),
-                  tap: () {
-                    QuickAlert.show(
-                      context: context,
-                      type: QuickAlertType.confirm,
-                      title: alertTitleLogOut,
-                      text: alertSubtitleLogOut,
-                      confirmBtnText: alertApprove,
-                      cancelBtnText: alertReject,
-                      confirmBtnColor: Theme.of(context).errorColor,
-                      onConfirmBtnTap: () async => await _authService.signOut(),
-                      onCancelBtnTap: () => Navigator.of(context).pop(false),
-                    );
-                  }),
-              const Divider(thickness: 1),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: CustomSwitch(
-                    switchValue: themeChange.darkTheme,
-                    switchOnChanged: (bool? value) {
-                      themeChange.darkTheme = value!;
-                    },
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
+        drawer: const CustomDrawer(),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
@@ -598,13 +399,14 @@ class Task extends State<TaskView> {
                                       borderRadius: BorderRadius.circular(16),
                                       border: Border.all(),
                                     ),
-                                    
+
                                     child: Padding(
                                       padding: const EdgeInsets.all(8.0),
                                       child: Row(
                                         crossAxisAlignment:
                                             CrossAxisAlignment.center,
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
                                         children: [
                                           Container(
                                             width: 48.0,
@@ -618,7 +420,8 @@ class Task extends State<TaskView> {
                                           Expanded(
                                             flex: 5,
                                             child: Column(
-                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
                                               children: [
                                                 Container(
                                                   //width: double.infinity,
