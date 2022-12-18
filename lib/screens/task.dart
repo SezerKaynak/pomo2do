8407b07272c84598e4e47cd7eda6806b4eeb551a
SezerKaynak <sezerkaynak00@gmail.com>
2022-12-18 19:41:20 +0300

    Shimmer added to task page

--- lib/screens/task.dart
+++ lib/screens/task.dart
@@ -19,6 +19,7 @@ import 'package:flutter_application_1/providers/pomodoro_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:collection/collection.dart';
 import 'package:quickalert/quickalert.dart';
+import 'package:shimmer/shimmer.dart';
 
 class TaskView extends StatefulWidget with ProjectThemeOptions {
   TaskView({Key? key}) : super(key: key);
@@ -572,7 +573,89 @@ class Task extends State<TaskView> {
                           ),
                         );
                       }
-                      return const Center(child: CircularProgressIndicator());
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Shimmer.fromColors(
+                                //period: Duration(milliseconds: 1),
+                                baseColor: Colors.grey[300]!,
+                                highlightColor: Colors.grey[100]!,
+                                enabled: true,
+                                child: ListView.separated(
+                                  separatorBuilder: (context, index) =>
+                                      const SizedBox(
+                                    height: 10,
+                                  ),
+                                  itemBuilder: (_, __) => Container(
+                                    // shape: RoundedRectangleBorder(
+                                    //   borderRadius: BorderRadius.circular(16),
+                                    // ),
+                                    alignment: Alignment.center,
+                                    height: 80,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(16),
+                                      border: Border.all(),
+                                    ),
+                                    
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Container(
+                                            width: 48.0,
+                                            height: 48.0,
+                                            color: Colors.white,
+                                          ),
+                                          const Padding(
+                                            padding: EdgeInsets.symmetric(
+                                                horizontal: 8.0),
+                                          ),
+                                          Expanded(
+                                            flex: 5,
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: [
+                                                Container(
+                                                  //width: double.infinity,
+                                                  height: 10.0,
+                                                  color: Colors.white,
+                                                ),
+                                                const Padding(
+                                                  padding: EdgeInsets.symmetric(
+                                                      vertical: 2.0),
+                                                ),
+                                                Container(
+                                                  height: 10.0,
+                                                  color: Colors.white,
+                                                ),
+                                                const Padding(
+                                                  padding: EdgeInsets.symmetric(
+                                                      vertical: 2.0),
+                                                ),
+                                                Container(
+                                                  width: 40.0,
+                                                  height: 10.0,
+                                                  color: Colors.white,
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  itemCount: 7,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
                     },
                   ),
                 ),
