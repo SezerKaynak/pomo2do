0bd332efa14cba9afa362d33882ba70a5beee116
SezerKaynak <sezerkaynak00@gmail.com>
2023-01-06 23:07:01 +0300

    A few customization

--- lib/views/pomodoro_view/widgets/task_info_list_tile.dart
+++ lib/views/pomodoro_view/widgets/task_info_list_tile.dart
@@ -14,33 +14,37 @@ class TaskInfoListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      trailing: Wrap(
-        crossAxisAlignment: WrapCrossAlignment.center,
-        children: [
-          const Icon(Icons.schedule, color: Colors.black),
-          const Icon(Icons.close, color: Colors.black),
-          Text(
-            pomodoroCount.toString(),
-            style: Theme.of(context)
-                .textTheme
-                .bodyMedium
-                ?.copyWith(fontSize: 22, color: Colors.black),
-          ),
-        ],
-      ),
+    return Card(
+      clipBehavior: Clip.hardEdge,
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8),
-          side: const BorderSide(color: Colors.black, width: 1)),
-      title: Text(taskName,
-          style: const TextStyle(color: Colors.black), maxLines: 1),
-      subtitle: Text(
-        taskInfo,
-        style: const TextStyle(color: Colors.black),
-        maxLines: 1,
-        overflow: TextOverflow.ellipsis,
+          side: BorderSide(color: Colors.grey.withOpacity(0.5), width: 1)),
+      elevation: 5,
+      child: ListTile(
+        trailing: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            const Icon(Icons.schedule, color: Colors.black),
+            const Icon(Icons.close, color: Colors.black),
+            Text(
+              pomodoroCount.toString(),
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium
+                  ?.copyWith(fontSize: 22, color: Colors.black),
+            ),
+          ],
+        ),
+        title: Text(taskName,
+            style: const TextStyle(color: Colors.black), maxLines: 1),
+        subtitle: Text(
+          taskInfo,
+          style: const TextStyle(color: Colors.black),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+        tileColor: Colors.blueGrey[50],
       ),
-      tileColor: Colors.blueGrey[50],
     );
   }
 }
