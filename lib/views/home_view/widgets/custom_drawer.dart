df92610bc5fdc5a0613bf43cd25701833214c276
SezerKaynak <sezerkaynak00@gmail.com>
2023-02-28 15:48:29 +0300

    update: Drawer header customization

--- lib/views/home_view/widgets/custom_drawer.dart
+++ lib/views/home_view/widgets/custom_drawer.dart
@@ -40,106 +40,89 @@ class _CustomDrawerState extends State<CustomDrawer> {
     final themeChange = Provider.of<DarkThemeProvider>(context);
     return Drawer(
       width: MediaQuery.of(context).size.width * 0.745,
-      child: Column(
+      child: ListView(
         children: [
           DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Colors.blue,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              image: const DecorationImage(
+                image: AssetImage("assets/images/header.png"),
+                fit: BoxFit.fitWidth,
               ),
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 0,
-                    child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: CircleAvatar(
-                              radius: 50.0,
-                              child: Consumer<DrawerImageProvider>(
-                                builder: (context, value, child) {
-                                  return drawerImageProvider.downloadUrl != null
-                                      ? CachedNetworkImage(
-                                          fit: BoxFit.cover,
-                                          imageUrl:
-                                              drawerImageProvider.downloadUrl!,
-                                          imageBuilder:
-                                              (context, imageProvider) {
-                                            return ClipOval(
-                                              child: SizedBox.fromSize(
-                                                size: const Size.fromRadius(50),
-                                                child: Image(
-                                                    image: imageProvider,
-                                                    fit: BoxFit.cover),
-                                              ),
-                                            );
-                                          },
-                                          placeholder: (context, url) {
-                                            return ClipOval(
-                                              child: SizedBox.fromSize(
-                                                size: const Size.fromRadius(20),
-                                                child:
-                                                    const CircularProgressIndicator(
-                                                        color: Colors.white),
-                                              ),
-                                            );
-                                          },
-                                          errorWidget: (context, url, error) =>
-                                              const Icon(Icons.error),
-                                        )
-                                      : ClipOval(
-                                          child: SizedBox.fromSize(
-                                            size: const Size.fromRadius(70),
-                                            child: Image.asset(
-                                              'assets/images/person.png',
-                                              fit: BoxFit.cover,
-                                            ),
-                                          ),
-                                        );
-                                },
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  child: Consumer<DrawerImageProvider>(
+                    builder: (context, value, child) {
+                      return drawerImageProvider.downloadUrl != null
+                          ? CachedNetworkImage(
+                              fit: BoxFit.cover,
+                              imageUrl: drawerImageProvider.downloadUrl!,
+                              imageBuilder: (context, imageProvider) {
+                                return ClipOval(
+                                  child: SizedBox.fromSize(
+                                    size: const Size.fromRadius(50),
+                                    child: Image(
+                                        image: imageProvider,
+                                        fit: BoxFit.cover),
+                                  ),
+                                );
+                              },
+                              placeholder: (context, url) {
+                                return ClipOval(
+                                  child: SizedBox.fromSize(
+                                    size: const Size.fromRadius(20),
+                                    child: const CircularProgressIndicator(
+                                        color: Colors.white),
+                                  ),
+                                );
+                              },
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                            )
+                          : ClipOval(
+                              child: SizedBox.fromSize(
+                                size: const Size.fromRadius(70),
+                                child: Image.asset(
+                                  'assets/images/person.png',
+                                  fit: BoxFit.cover,
+                                ),
                               ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 8.0),
-                              child: StreamBuilder(
-                                  stream: user.snapshots(),
-                                  builder: (BuildContext context,
-                                      AsyncSnapshot asyncSnapshot) {
-                                    if (asyncSnapshot.hasError) {
-                                      return const Text(
-                                          "Bir şeyler yanlış gitti");
-                                    } else if (asyncSnapshot.hasData &&
-                                        !asyncSnapshot.data!.exists) {
-                                      return const Text(
-                                        "Hesap ayarları sayfasından profil resmi seçebilirsiniz",
-                                        overflow: TextOverflow.clip,
-                                        maxLines: 3,
-                                      );
-                                    } else if (asyncSnapshot.connectionState ==
-                                        ConnectionState.active) {
-                                      return Text(
-                                        "${asyncSnapshot.data.data()["name"]}"
-                                        " ${asyncSnapshot.data.data()["surname"]}",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headline4
-                                            ?.copyWith(
-                                                fontWeight: FontWeight.w600,
-                                                fontSize: 26,
-                                                color: Colors.black),
-                                      );
-                                    }
-                                    return const Text("Loading");
-                                  }),
-                            ),
-                          ),
-                        ]),
+                            );
+                    },
                   ),
-                ],
-              )),
+                ),
+                StreamBuilder(
+                    stream: user.snapshots(),
+                    builder:
+                        (BuildContext context, AsyncSnapshot asyncSnapshot) {
+                      if (asyncSnapshot.hasError) {
+                        return const Text("Bir şeyler yanlış gitti");
+                      } else if (asyncSnapshot.hasData &&
+                          !asyncSnapshot.data!.exists) {
+                        return const Text(
+                          "Hesap ayarları sayfasından profil resmi seçebilirsiniz",
+                          overflow: TextOverflow.clip,
+                          maxLines: 3,
+                        );
+                      } else if (asyncSnapshot.connectionState ==
+                          ConnectionState.active) {
+                        return Text(
+                          "${asyncSnapshot.data.data()["name"]}"
+                          " ${asyncSnapshot.data.data()["surname"]}",
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 20),
+                        );
+                      }
+                      return const Text("Loading");
+                    }),
+              ],
+            ),
+          ),
           settings.Settings(
             settingIcon: Icons.account_circle,
             title: settingTitle(context, "Hesap Ayarları"),
@@ -196,15 +179,13 @@ class _CustomDrawerState extends State<CustomDrawer> {
                 );
               }),
           const Divider(thickness: 1),
-          Expanded(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: CustomSwitch(
-                switchValue: themeChange.darkTheme,
-                switchOnChanged: (bool? value) {
-                  themeChange.darkTheme = value!;
-                },
-              ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: CustomSwitch(
+              switchValue: themeChange.darkTheme,
+              switchOnChanged: (bool? value) {
+                themeChange.darkTheme = value!;
+              },
             ),
           )
         ],
