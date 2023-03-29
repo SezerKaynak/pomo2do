edca3fa1c2601f8b4419c5d8ce213999e7ffb775
gokalpakoglu <gokalpakoglu@gmail.com>
2023-03-29 17:38:12 +0300

    add: Multi language support (L10n)

---
+++ lib/l10n/app_l10n_tr.dart
@@ -0,0 +1,477 @@
+import 'app_l10n.dart';
+
+/// The translations for Turkish (`tr`).
+class L10nTr extends L10n {
+  L10nTr([String locale = 'tr']) : super(locale);
+
+  @override
+  String get welcome => 'Hoşgeldiniz!';
+
+  @override
+  String get firstSignIn => 'Lütfen öncelikle giriş yapınız👋';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get login => 'Giriş Yap';
+
+  @override
+  String get password => 'Şifre';
+
+  @override
+  String get forgotPassword => 'Şifremi Unuttum';
+
+  @override
+  String get emailText => 'abc@xyz.com';
+
+  @override
+  String get enterPassword => 'Şifrenizi Girin';
+
+  @override
+  String get loginWithAccount => 'Hesabınızla Giriş Yapın';
+
+  @override
+  String get dontHaveAccount => 'Henüz bir hesabınız yok mu?';
+
+  @override
+  String get emailAlert => 'E-Posta Alanı Boş Bırakılamaz!';
+
+  @override
+  String get emailAlertSubtitle => 'Lütfen e-postanızı girin.';
+
+  @override
+  String get passwordAlert => 'Şifre Alanı Boş Bırakılamaz!';
+
+  @override
+  String get passwordAlertSubtitle => 'Lütfen şifrenizi girin.';
+
+  @override
+  String get userNotFound => 'Kullanıcı Bulunamadı!';
+
+  @override
+  String get userNotFoundSubtitle => 'Hesabınız yoksa aşağıdaki kayıt ol butonunu kullanarak kayıt olabilirsiniz.';
+
+  @override
+  String get wrongPassword => 'Şifre Yanlış!';
+
+  @override
+  String get wrongPasswordSubtitle => 'Şifrenizi yanlış girdiniz, lütfen tekrar deneyin...';
+
+  @override
+  String get enterEmailHint => 'E-posta adresinizi girin.';
+
+  @override
+  String get enterEmail => 'Şifresini sıfırlamak istediğiniz hesabınızın e-mail adresini girin:';
+
+  @override
+  String get checkEmail => 'E-posta adresinizi kontrol edin.';
+
+  @override
+  String get invalidEmail => 'Geçersiz E-Mail Adresi!';
+
+  @override
+  String get invalidEmailSubtitle => 'Lütfen geçerli bir E-Mail adresi girin.';
+
+  @override
+  String get register => 'Kayıt Ol!';
+
+  @override
+  String get subtitle2 => 'Aşağıdaki alanları doldurarak kaydolabilirsiniz.🙂';
+
+  @override
+  String get name => 'Ad';
+
+  @override
+  String get surname => 'Soyad';
+
+  @override
+  String get yourName => 'Adınız';
+
+  @override
+  String get yourSurname => 'Soyadınız';
+
+  @override
+  String get yourBirthday => 'Doğum Tarihiniz';
+
+  @override
+  String get weakPassword => 'Güçsüz Şifre!';
+
+  @override
+  String get weakPasswordSubtitle => 'Girdiğiniz şifre minimum 6 haneden oluşmalı!';
+
+  @override
+  String get emailAlreadyInUse => 'Geçersiz E-Posta!';
+
+  @override
+  String get emailAlreadyInUseSubtitle => 'Girdiğiniz E-posta adresi başka bir hesaba bağlı!';
+
+  @override
+  String get nameAlert => 'İsim Alanı Boş Bırakılamaz!';
+
+  @override
+  String get nameAlertSubtitle => 'Lütfen isminizi girin.';
+
+  @override
+  String get surnameAlert => 'Soy İsim Alanı Boş Bırakılamaz!';
+
+  @override
+  String get surnameAlertSubtitle => 'Lütfen soy isminizi girin.';
+
+  @override
+  String get birthdayAlert => 'Doğum Tarihi Alanı Boş Bırakılamaz!';
+
+  @override
+  String get birthdayAlertSubtitle => 'Lütfen doğum tarihinizi seçin.';
+
+  @override
+  String get confirmButtonText => 'Kapat';
+
+  @override
+  String get trashAlertTitle => 'Görev Çöp Kutusuna Taşınacak!';
+
+  @override
+  String get trashAlertSubtitle => 'Görev çöp kutusuna taşınsın mı?';
+
+  @override
+  String get alertApprove => 'Onayla';
+
+  @override
+  String get alertReject => 'İptal Et';
+
+  @override
+  String get alertTitleLogOut => 'Çıkış Yapılacak!';
+
+  @override
+  String get alertSubtitleLogOut => 'Çıkış yapmak istediğinizden emin misiniz?';
+
+  @override
+  String get changePassword => 'Şifreyi Değiştir';
+
+  @override
+  String get rePassword => 'Aşağıdaki alanları doldurarak şifrenizi yenileyebilirsiniz 🙂';
+
+  @override
+  String get oldPassword => 'Eski Şifre';
+
+  @override
+  String get oldPasswordHint => 'Eski Şifrenizi Girin';
+
+  @override
+  String get newPasswordText => 'Yeni Şifre';
+
+  @override
+  String get passwordHint => 'Yeni Şifrenizi Girin';
+
+  @override
+  String get oldPasswordAlert => 'Eski Şifre Alanı Boş Bırakılamaz!';
+
+  @override
+  String get oldPasswordAlertSubtitle => 'Lütfen eski şifrenizi girin.';
+
+  @override
+  String get newPasswordAlert => 'Yeni Şifre Alanı Boş Bırakılamaz!';
+
+  @override
+  String get newPasswordAlertSubtitle => 'Lütfen yeni şifrenizi girin.';
+
+  @override
+  String get passwordConfirmed => 'Şifreniz Başarıyla Değiştirildi.';
+
+  @override
+  String get passwordConfirmedSubtitle => 'Lütfen yeni şifrenizi kullanarak tekrar giriş yapınız.';
+
+  @override
+  String get taskPageTitle => 'Görev Düzenleme Sayfası';
+
+  @override
+  String get taskPageSubtitle => 'Görevin ismi,türü ve açıklamasını düzenleyebilirsiniz👋';
+
+  @override
+  String get taskPageTaskName => 'Görev İsmi';
+
+  @override
+  String get taskPageTaskType => 'Görev Türü';
+
+  @override
+  String get taskPageTaskInfo => 'Görev Açıklaması';
+
+  @override
+  String get isTaskDone => 'Görev tamamlandı mı?';
+
+  @override
+  String get isTaskArchive => 'Görev arşivlensin mi?';
+
+  @override
+  String get taskMovedIntoArchive => 'Görev arşive taşındı!';
+
+  @override
+  String get taskMovedIntoCompleted => 'Görev tamamlanmış görevler sayfasına taşındı!';
+
+  @override
+  String get updateButtonText => 'Güncelle';
+
+  @override
+  String get pomodoroTitle => 'Pomodoro Ayarları';
+
+  @override
+  String get pomodoroSubtitle => 'Aşağıdaki alanlardan pomodoro zamanlayıcısının ayarlarını yapabilirsiniz.🙂';
+
+  @override
+  String get workTimer => 'Çalışma Zamanı Süresi';
+
+  @override
+  String get breakTimer => 'Mola Süresi';
+
+  @override
+  String get longBreakTimer => 'Uzun Mola Süresi';
+
+  @override
+  String get longBreakNumber => 'Uzun Molanın Kaçıncı Arada Verileceği';
+
+  @override
+  String get workTimerSelect => 'Çalışma Zamanı Süresini Seçiniz';
+
+  @override
+  String get breakTimerSelect => 'Mola Süresini Seçiniz';
+
+  @override
+  String get longBreakTimerSelect => 'Uzun Mola Süresini Seçiniz';
+
+  @override
+  String get longBreakNumberSelect => 'Uzun Molanın Kaçıncı Arada Verileceğini Seçiniz';
+
+  @override
+  String get minute => 'dakika';
+
+  @override
+  String get noResult => 'Sonuç Bulunamadı!';
+
+  @override
+  String get moveIntoTrash => 'ÇÖP KUTUSUNA TAŞI';
+
+  @override
+  String get editText => 'DÜZENLE';
+
+  @override
+  String get noActiveTask => 'Aktif görev bulunamadı!';
+
+  @override
+  String get noTask => 'Görev bulunamadı!';
+
+  @override
+  String get addButtonText => 'Ekle';
+
+  @override
+  String get editProfileTitle => 'Profil Düzenleme';
+
+  @override
+  String get editProfileSubtitle => 'Kişisel bilgilerinizi düzenleyebilirsiniz👋';
+
+  @override
+  String get buttonText => 'Kaydet';
+
+  @override
+  String get statisticsTitle => 'İstatistikler';
+
+  @override
+  String get taskFound => 'Görev bulundu!';
+
+  @override
+  String get taskFoundDetailed => 'Aynı isimli başka bir görev bulunuyor, lütfen görev adını değiştirin.';
+
+  @override
+  String get loginSpotify => 'Spotify\'a giriş yapmalısınız.';
+
+  @override
+  String get noSpotify => 'Spotify uygulaması bulunamadı.';
+
+  @override
+  String get reSignIn => 'Lütfen tekrar giriş yapmayı deneyin.';
+
+  @override
+  String get noSignIn => 'Giriş yapılamadı';
+
+  @override
+  String get noArchiveTask => 'Arşivlenmiş görev bulunamadı!';
+
+  @override
+  String get moveDonePage => 'görevi tamamlanmış görevler sayfasına taşındı!';
+
+  @override
+  String get moveTaskPage => 'görevi görevler sayfasına taşındı!';
+
+  @override
+  String get moveArchive => 'Seçili görevleri arşivden çıkar';
+
+  @override
+  String get archivedTask => 'Arşivlenmiş Görevler';
+
+  @override
+  String get errorText => 'Bir hata oldu';
+
+  @override
+  String get moveTrashBin => 'Görev çöp kutusuna taşındı!';
+
+  @override
+  String get noDoneTask => 'Tamamlanmış görev bulunamadı!';
+
+  @override
+  String get selectUndone => 'Seçili görevleri tamamlanmamış olarak işaretle';
+
+  @override
+  String get doneTask => 'Tamamlanmış Görevler';
+
+  @override
+  String get noTaskTrash => 'Çöp kutusunda görev bulunamadı!';
+
+  @override
+  String get activeButton => 'Aktif Et';
+
+  @override
+  String get deleteButton => 'Sil';
+
+  @override
+  String get trash => 'Çöp Kutusu';
+
+  @override
+  String get somethingWrong => 'Bir şeyler yanlış gitti';
+
+  @override
+  String get loading => 'Yükleniyor';
+
+  @override
+  String get noPic => 'Resim seçilemedi!';
+
+  @override
+  String get selectPic => 'Profil Resmi Seçin';
+
+  @override
+  String get camera => 'Kamera';
+
+  @override
+  String get gallery => 'Galeri';
+
+  @override
+  String get addTask => 'Görev Ekle';
+
+  @override
+  String get uSelectPic => 'Hesap ayarları sayfasından profil resmi seçebilirsiniz';
+
+  @override
+  String get accSettings => 'Hesap Ayarları';
+
+  @override
+  String get uEditProfile => 'Profilinizi düzenleyebilirsiniz.';
+
+  @override
+  String get uChangePassword => 'Şifrenizi değiştirebilirsiniz.';
+
+  @override
+  String get uEditNotification => 'Bildirim ayarlarını yapabilirsiniz.';
+
+  @override
+  String get notificationSettings => 'Bildirim Ayarları';
+
+  @override
+  String get uEditPomodoro => 'Pomodoro sayacı vb. ayarları yapabilirsiniz.';
+
+  @override
+  String get logOutAcc => 'Hesaptan çıkış yapın.';
+
+  @override
+  String get logOut => 'Çıkış Yap';
+
+  @override
+  String get taskBeCompleted => 'Tamamlanması Gereken Görevler';
+
+  @override
+  String get passingTime => 'Görevlerde Bugün Geçen Süre';
+
+  @override
+  String get pomoTodo => 'PomoTodo';
+
+  @override
+  String get leaderBoard => 'Liderlik Sıralaması';
+
+  @override
+  String get weekly => 'Haftalık';
+
+  @override
+  String get monthly => 'Aylık';
+
+  @override
+  String get longBreak1 => '1.arada uzun mola';
+
+  @override
+  String get longBreak5 => '5.arada uzun mola';
+
+  @override
+  String get failConnect => 'Bağlanamadı!';
+
+  @override
+  String get connectSpotify => 'Spotify\'a Bağlan';
+
+  @override
+  String get selectDone => 'Tamamlandı Olarak İşaretle';
+
+  @override
+  String get resetPomodoro => 'Pomodoro Sayacını Sıfırla';
+
+  @override
+  String get pomodoro => 'Pomodoro';
+
+  @override
+  String get uSure => 'Emin misin?';
+
+  @override
+  String get pomodoroWillReset => 'Pomodoro sayacı sıfırlanacak!';
+
+  @override
+  String get shortBreak => 'Kısa Ara';
+
+  @override
+  String get longBreak => 'Uzun Ara';
+
+  @override
+  String get openSong => 'Spotify\'a bağlandı, uygulamadan şarkıyı açın.';
+
+  @override
+  String get errorPic => 'Fotoğraf alınırken hata oluştu...';
+
+  @override
+  String get takingPic => 'Fotoğraf alınıyor...';
+
+  @override
+  String get failedSignIn => 'Giriş Başarısız!';
+
+  @override
+  String get workTime => 'Çalışma Süresi';
+
+  @override
+  String get few => 'Az';
+
+  @override
+  String get much => 'Çok';
+
+  @override
+  String get monday => 'Pzt';
+
+  @override
+  String get tuesday => 'Sal';
+
+  @override
+  String get wednesday => 'Çar';
+
+  @override
+  String get thursday => 'Per';
+
+  @override
+  String get friday => 'Cum';
+
+  @override
+  String get saturday => 'Cmt';
+
+  @override
+  String get sunday => 'Paz';
+}
